4fa0be9aa34846b4ff5a365786a81bef2fec3793
Takafumi Iju <takafumi.iju@sk.com>
2017-02-21 16:08:45 +0900

    Advanced a little.
    * Create grid cells in Code behind.

--- XamarinLifeGameXAML/LgViewModel.cs
+++ XamarinLifeGameXAML/LgViewModel.cs
@@ -1,43 +1,9 @@
-﻿using System;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Windows.Input;
-using Xamarin.Forms;
-
-namespace XamarinLifeGameXAML
+﻿namespace XamarinLifeGameXAML
 {
-    public class LgViewModel : INotifyPropertyChanged
+    public class LgViewModel
     {
-        private const int CellSize = 9;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
         public LgViewModel()
         {
-            var cells = new Cell[CellSize * CellSize];
-
-            for (var i = 0; i < CellSize; i++)
-            {
-                for (var j = 0; j < CellSize; j++)
-                {
-                    var index = (j + i * CellSize);
-                    cells[index] = new Cell {
-                        State = 0,
-                        IndexX = i,
-                        IndexY = j,
-                        Index =  index,
-                        Text = index.ToString()
-                    };
-                }
-            }
-
-            Cells = cells;
-        }
-
-        public Cell[] Cells
-        {
-            get;
-            private set;
         }
     }
 }
--- XamarinLifeGameXAML/XamarinLifeGameXAMLPage.xaml.cs
+++ XamarinLifeGameXAML/XamarinLifeGameXAMLPage.xaml.cs
@@ -6,20 +6,68 @@ namespace XamarinLifeGameXAML
 {
     public partial class XamarinLifeGameXAMLPage : ContentPage
     {
+        private const int CellSize = 9;
+
+        private Cell[] _cells;
+
         public XamarinLifeGameXAMLPage()
         {
             InitializeComponent();
+
+            var RowDefinitions = new RowDefinition[CellSize];
+            var RowCollections = new RowDefinitionCollection();
+            for (var i = 0; i < CellSize; i++)
+            {
+                RowDefinitions[i] = new RowDefinition { Height = GridLength.Star};
+                RowCollections.Add(RowDefinitions[i]);
+            }
+
+            var ColumnDefinitions = new ColumnDefinition[CellSize];
+            var ColumnCollections = new ColumnDefinitionCollection();
+            for (var i = 0; i < CellSize; i++)
+            {
+                ColumnDefinitions[i] = new ColumnDefinition { Width = GridLength.Star };
+                ColumnCollections.Add(ColumnDefinitions[i]);
+            }
+
+            CellGrid.RowDefinitions = RowCollections;
+            CellGrid.ColumnDefinitions = ColumnCollections;
+            CellGrid.Padding = new Thickness(5, Device.OnPlatform(20, 0, 0), 5, 0);
+
+            _cells = new Cell[CellSize * CellSize];
+            var tgr = new TapGestureRecognizer();
+            tgr.Tapped += (sender, e) => CellClicked(sender, e);
+
+            for (var i = 0; i < CellSize; i++)
+            {
+                for (var j = 0; j < CellSize; j++)
+                {
+                    var index = (j + i * CellSize);
+                    _cells[index] = new Cell {
+                        State = 0,
+                        IndexX = i,
+                        IndexY = j,
+                        Index =  index,
+                        Text = index.ToString(),
+                        FontSize = 20,
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        VerticalTextAlignment = TextAlignment.Center,
+                        GestureRecognizers = { tgr }
+                    };
+                    CellGrid.Children.Add(_cells[index], i, j);
+                }
+            }
         }
 
-        public void Tapped2(object sender, EventArgs e)
+        public void CellClicked(object sender, EventArgs e)
         {
             var vm = (LgViewModel) this.BindingContext;
-            Cell cell = (Cell)sender;
+            var cell = (Cell)sender;
 
-            int index = cell.Index;
+            var index = cell.Index;
 
             Debug.WriteLine(index);
-            vm.Cells[index].ChangeState();
+            _cells[index].ChangeState();
         }
     }
 }
