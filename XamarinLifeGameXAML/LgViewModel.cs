49904cf73e94ba3720edb09eb8fb86d83a476dfa
Takafumi Iju <takafumi.iju@sk.com>
2017-02-21 15:44:09 +0900

    Advanced a little.
    * Revert StackLayout to Grid.

--- XamarinLifeGameXAML/LgViewModel.cs
+++ XamarinLifeGameXAML/LgViewModel.cs
@@ -34,30 +34,6 @@ namespace XamarinLifeGameXAML
             Cells = cells;
         }
 
-        public Cell[] Cells0 => GetCells(0);
-        public Cell[] Cells1 => GetCells(1);
-        public Cell[] Cells2 => GetCells(2);
-        public Cell[] Cells3 => GetCells(3);
-        public Cell[] Cells4 => GetCells(4);
-        public Cell[] Cells5 => GetCells(5);
-        public Cell[] Cells6 => GetCells(6);
-        public Cell[] Cells7 => GetCells(7);
-        public Cell[] Cells8 => GetCells(8);
-
-        private Cell[] GetCells(int colIndex)
-        {
-            var cells = new Cell[CellSize];
-            foreach (var cell in Cells)
-            {
-                if (cell.IndexX == colIndex)
-                {
-                    cells[cell.IndexY] = cell;
-                }
-            }
-
-            return cells;
-        }
-
         public Cell[] Cells
         {
             get;
